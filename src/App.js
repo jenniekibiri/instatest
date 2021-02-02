cd875cc8f4560a1635bfb0fa509f833a50365429
jenniekibiri <josphatje@gmail.com>
2021-02-02 19:13:09 +0300

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <p>place your Component here</p>
+        {/* <p>place your Component here</p> */}
+        <Navbar/>
+        
       </div>
     )
   }
